60b47cbc7e6fa940938e1d3d1116de49449e6be0
iqq-0906 <camellia09030713@gmail.com>
2024-11-28 17:34:04 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -383,7 +383,7 @@ key = random.PRNGKey(0)
 
 K=2.411
 P =4500 # number of output sensors, 100 for each side
-Q =4500  # number of collocation points for each input sample
+Q =3000  # number of collocation points for each input sample
 M = 5000
 r =0.025610
 v=0.165856529
