d8ee6285c0730f309e9d941d1f1fb0defa6aa6b8
iqq-0906 <camellia09030713@gmail.com>
2024-11-28 17:20:01 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -422,8 +422,8 @@ print("outputs_i shape:", outputs_i.shape)
 dataset1 = TensorDataset(x_i,t_i,outputs_i)
 dataset2 = TensorDataset(x_b,t_b,outputs_b)
 # dataset3 = TensorDataset(x_bc4,t_bc4,s_bc4)
-batch_size1= 1000
-batch_size2= 1000
+batch_size1= 3000
+batch_size2= 3000
 dataloader1 = DataLoader(dataset1, batch_size=batch_size1, shuffle=True)
 dataloader2 = DataLoader(dataset2, batch_size=batch_size2, shuffle=True)
 # dataloader3 = DataLoader(dataset3, batch_size=batch_size2, shuffle=True)
