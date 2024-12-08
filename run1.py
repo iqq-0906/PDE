5d139489bf320b5cca67969051ff47df0e280bce
iqq-0906 <camellia09030713@gmail.com>
2024-12-08 10:09:29 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -123,7 +123,7 @@ class PI_DeepONet(nn.Module):
         s_xx =(hessian(self.operator_net,argnums=4)(u1,u2,u_s1,u_s2,x,t).sum(dim=0)).sum(dim=0).to(device)
         s_t =jacrev(self.operator_net,argnums=5)(u1,u2,u_s1,u_s2,x,t).sum(dim=0).to(device)
         member1 = torch.tensor(0.5, device='cuda')
-        member2 = torch.tensor(0.1659, device='cuda')
+        member2 = torch.tensor(0.3, device='cuda')
         member3 = torch.tensor(0.0267, device='cuda')
         res =s_t-(member1)*(member2**2)*(x**2)*s_xx-member3*x*s_x+member3*s
         return res
