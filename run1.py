b6c40adaecd708b445037de268b250dcf1465500
iqq-0906 <camellia09030713@gmail.com>
2024-12-10 17:32:01 +0800

    Update run1.py

--- run1.py
+++ run1.py
@@ -427,7 +427,7 @@ def generate_one_training_data(key,P,Q,K,M,r,v,T):
 key = random.PRNGKey(0)
 
 K=2.411
-P =6000 # number of output sensors, 100 for each side
+P =9000 # number of output sensors, 100 for each side
 Q =1000  # number of collocation points for each input sample
 M = 5000
 r =0.025610
@@ -478,7 +478,7 @@ dataloader2 = DataLoader(dataset2, batch_size=batch_size2, shuffle=True)
 model1 =KAN([3,2,1], base_activation=nn.Identity)
 model2 = KAN([3,2,1], base_activation=nn.Identity)
 # model3 = KAN([2,1], base_activation=nn.Identity)
-model4 = KAN([2000,2,1], base_activation=nn.Identity)
+model4 = KAN([3000,2,1], base_activation=nn.Identity)
 model5 = KAN([2,6,1], base_activation=nn.Identity)
 
 # model1 =BayesianNetwork()
